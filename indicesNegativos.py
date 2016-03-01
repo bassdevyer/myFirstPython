dcbf575fd860d9eb102dc33553dd13068b54110e
miguetv17 <miguetv17@gmail.com>
2016-03-01 01:19:00 -0500

    Índices negativos

---
+++ indicesNegativos.py
@@ -0,0 +1,8 @@
+lista = ["Sara", "Pedro", 98, 76, 'Juan', "David", 89]
+
+#Accediendo al ultimo elemento de la lista, sin saber el tamaño
+print(lista[-1])
+
+#Puedo ir recorriendo la lista de atras para adelante
+print(lista[-4])
+
