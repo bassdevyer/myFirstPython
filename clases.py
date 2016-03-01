f8843673847752389638cc4178ed72892e612086
miguetv17 <miguetv17@gmail.com>
2016-03-01 01:46:59 -0500

    atributos

--- clases.py
+++ clases.py
@@ -1,7 +1,16 @@
 class Persona:
+    nBrazos = 0
+    nPiernas = 0
+    cabello = True
+    cCabello = "Defecto"
     #Las clases no se pueden quedar vacias, cuando las estas definiendo debes implementar el codigo
     #Para saltar la implementacion utiliza pass
-    pass
+    #pass
 
 class Hombre:
-    pass
+    nombre = "Defecto"
+    sexo = "M"
+
+class Mujer:
+    nombre = "Defecto"
+    sexo = "F"
