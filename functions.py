12291f6d54a4dcd4c410dc6f8bd3bf42c561ce5e
miguetv17 <miguetv17@gmail.com>
2016-03-01 00:40:54 -0500

    Funciones

---
+++ functions.py
@@ -0,0 +1,13 @@
+def saludo():
+    # Cuando no se desea definir por ahora, se utiliza pass
+    # pass
+    print("Hola")
+
+def comer():
+    print("Comer")
+
+saludo()
+saludo()
+saludo()
+saludo()
+saludo()
