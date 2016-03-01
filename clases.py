333222443c9fb5716659ed9650f620645467c98f
miguetv17 <miguetv17@gmail.com>
2016-03-01 01:44:57 -0500

    clases

---
+++ clases.py
@@ -0,0 +1,7 @@
+class Persona:
+    #Las clases no se pueden quedar vacias, cuando las estas definiendo debes implementar el codigo
+    #Para saltar la implementacion utiliza pass
+    pass
+
+class Hombre:
+    pass
