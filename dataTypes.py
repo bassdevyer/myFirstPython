083ceece0e95b96dfc7120ee825dc704335c0cf2
miguetv17 <miguetv17@gmail.com>
2016-02-29 22:02:32 -0500

    Initial commit

---
+++ dataTypes.py
@@ -0,0 +1,11 @@
+edad = 27
+
+altura = 1.73
+
+nombre = "Miguel"
+nombre1 = 'Miguel'
+
+sexoMasculino = True
+
+numeroComplejo = 7+8j
+
---
+++ operators.py
@@ -0,0 +1,36 @@
+x = 10
+y = 2
+
+resultado = x + y
+
+print(resultado)
+
+resultado = x - y
+
+print(resultado)
+
+resultado = x * y
+
+print(resultado)
+
+resultado = x / y
+
+print(resultado)
+
+# El doble slash redondea hacia abajo
+resultado = x // y
+
+print(resultado)
+
+
+#Simbolo de porcentaje, módulo
+resultado = x % y
+
+print(resultado)
+
+
+# potencia
+
+resultado = x ** y
+
+print(resultado)
