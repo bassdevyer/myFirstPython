39486254c499de049c1897145dc8659b665e886b
miguetv17 <miguetv17@gmail.com>
2016-03-01 00:18:44 -0500

    condicionales anidados

--- conditionalIf.py
+++ conditionalIf.py
@@ -1,8 +1,13 @@
-edad = 17
+edad = 19
+pago = False
 
 #Los parentesis son opcionales
 if(edad > 18):
     print('Sí es mayor de edad')
+    if pago == True:
+        print('Entonces eres mayor y pagaste')
+    else:
+        print("Eres mayor de edad y no pagaste")
 else:
     #La identacion es parte básica de python
     print("No es mayor de edad")
