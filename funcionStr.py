842b0ae9c5aec6475358395706f185bd126d83d1
miguetv17 <miguetv17@gmail.com>
2016-03-01 01:32:19 -0500

    Funcion str

---
+++ funcionStr.py
@@ -0,0 +1,7 @@
+numero = 10
+numero2 = 20
+print(numero + numero2)
+# Si a esto se le aplica str
+print(str(numero) + str(numero2))
+
+# A veces vamos a tener la necesidad de tratar numeros como cadenas
