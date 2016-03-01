13b44bec421639b201f97234c759e1bb35288bbb
miguetv17 <miguetv17@gmail.com>
2016-03-01 00:58:58 -0500

    tuplas

---
+++ tuplas.py
@@ -0,0 +1,11 @@
+#Tupla que contiene tres elementos
+tupla=(26, 1.70, "Miguel")
+
+#print(tupla[2])
+
+indice = 0
+
+#Para saber el tamaño de la tupla usamos len
+while indice < len(tupla):
+    print(tupla[indice])
+    indice = indice + 1
