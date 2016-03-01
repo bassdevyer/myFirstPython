7606a560b7001bfff4e94fd4d8c21f1f2ae1e780
miguetv17 <miguetv17@gmail.com>
2016-03-01 02:03:14 -0500

    Escribir archivos

--- archivos.py
+++ archivos.py
@@ -2,4 +2,13 @@ def crearArchivo():
     archivo=open("datos.txt", "w")
     archivo.close()
 
-crearArchivo()
+def escribirArchivo():
+    #modo a = append
+    archivo = open('datos.txt', 'a')
+    archivo.write("Miguel Torres\n")
+    archivo.write("9203929393")
+    archivo.close()
+
+
+#crearArchivo()
+escribirArchivo()
