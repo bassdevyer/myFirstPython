7859141a21d3908646ae5701adeba4dda799519e
miguetv17 <miguetv17@gmail.com>
2016-03-01 01:04:30 -0500

    Porciones de tuplas

---
+++ porcionesDeTuplas.py
@@ -0,0 +1,11 @@
+tupla = (12, 13, 14, 56, 67, 78)
+
+# A partir de la posicion 0 quiero 5 lugares
+tupla2 = tupla[3:5]
+print(tupla2)
+
+tupla2 = tupla[:5]
+print(tupla2)
+
+tupla2 = tupla[3:]
+print(tupla2)
